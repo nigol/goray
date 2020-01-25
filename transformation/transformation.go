daa107d0525928d700ee3a02db09cda291f4d9e4
Martin Polak <nigol@nigol.cz>
2020-01-25 10:31:46 +0000

    Transformation: scaling.

--- transformation/transformation.go
+++ transformation/transformation.go
@@ -13,3 +13,13 @@ func Translation(x float64, y float64, z float64) matrix.Matrix {
 			{0, 0, 0, 1},
 		}}
 }
+
+func Scaling(x float64, y float64, z float64) matrix.Matrix {
+	return matrix.Matrix{4, 4,
+		[][]float64{
+			{x, 0, 0, 0},
+			{0, y, 0, 0},
+			{0, 0, z, 0},
+			{0, 0, 0, 1},
+		}}
+}
--- transformation/transformation_test.go
+++ transformation/transformation_test.go
@@ -29,3 +29,36 @@ func TestMulTranslationMatrixVector(t *testing.T) {
 		t.Errorf("Mul translation changed vector.")
 	}
 }
+
+func TestMulScalingMatrix(t *testing.T) {
+	p := tuple.CreatePoint(-4, 6, 8)
+	tr := Scaling(2, 3, 4)
+	if !tuple.CreatePoint(-8, 18, 32).Equal(tr.MulTuple(p)) {
+		t.Errorf("Mul scaling went wrong.")
+	}
+}
+
+func TestMulScalingMatrixVector(t *testing.T) {
+	v := tuple.CreateVector(-4, 6, 8)
+	tr := Scaling(2, 3, 4)
+	if !tuple.CreateVector(-8, 18, 32).Equal(tr.MulTuple(v)) {
+		t.Errorf("Mul scaling vector went wrong.")
+	}
+}
+
+func TestMulScalingInverseMatrixVector(t *testing.T) {
+	v := tuple.CreateVector(-4, 6, 8)
+	tr := Scaling(2, 3, 4)
+	inv := tr.Inverse4x4()
+	if !tuple.CreateVector(-2, 2, 2).Equal(inv.MulTuple(v)) {
+		t.Errorf("Mul inverse scaling vector went wrong.")
+	}
+}
+
+func TestMulScalingMatrixReflection(t *testing.T) {
+	p := tuple.CreatePoint(2, 3, 4)
+	tr := Scaling(-1, 1, 1)
+	if !tuple.CreatePoint(-2, 3, 4).Equal(tr.MulTuple(p)) {
+		t.Errorf("Mul scaling reflection went wrong.")
+	}
+}
